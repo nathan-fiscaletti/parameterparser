754fe5b4469b024ab9c5b23c2bd366ffaf5a6d2b
Nathan Fiscaletti <nathhan.fiscaletti@gmail.com>
2016-11-07 10:43:43 -0800

    Fixed Style CI

--- src/ParameterParser/ParameterParser.php
+++ src/ParameterParser/ParameterParser.php
@@ -51,7 +51,7 @@ class ParameterParser
                 }
                 $this->argv[count($this->argv) - 1] .=
                 ' '.substr($argument_part, 0, strlen($argument_part) - 1);
-            } else if (substr($argument, 0, 1) == '"') {
+            } elseif (substr($argument, 0, 1) == '"') {
                 $this->argv[] = substr($argument, 1);
                 while (
                     ($argument_part = array_shift($argv)) != null &&
