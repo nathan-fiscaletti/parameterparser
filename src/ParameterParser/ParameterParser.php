530a7934e9b36df56e0dcdc78c3d965582a18568
Nathan Fiscaletti <nathan.fiscaletti@stackpath.com>
2016-11-07 19:38:27 -0800

    Fixed issue with quote parameter parsing, Removed parsePrefixOnly function, modified parse function to accept variadic functions, updated examples

--- src/ParameterParser/ParameterParser.php
+++ src/ParameterParser/ParameterParser.php
@@ -31,55 +31,7 @@ class ParameterParser
     }
 
     /**
-     * Parse the parameters counting any parameter that's not prefixed as an
-     * argument for the last prefixed parameter.
-     *
-     * @return array
-     */
-    public function parsePrefixOnly()
-    {
-        $results = [];
-
-        $i = 0;
-        while ($i < count($this->argv)) {
-            $parameter = $this->argv[$i];
-            if ($this->prefixExists($parameter)) {
-                $closure = $this->getClosure($parameter);
-                $prefix = $this->getPrefix($parameter);
-                $closure_arguments = [
-                    substr(
-                        $parameter,
-                        strlen($prefix),
-                        strlen($parameter) - strlen($prefix)
-                    ),
-                ];
-                $i++;
-                while (
-                    isset($this->argv[$i]) &&
-                    ($argument = $this->argv[$i]) != null &&
-                    ! $this->prefixExists($argument)
-                ) {
-                    $closure_arguments[] = $argument;
-                    $i++;
-                }
-                $results[
-                    substr(
-                        $parameter,
-                        strlen($prefix),
-                        strlen($parameter) - strlen($prefix)
-                    )
-                ] = $closure(...$closure_arguments);
-            } else {
-                $results['default'] = $this->prefixes->default->call($this, $parameter);
-                $i++;
-            }
-        }
-
-        return $results;
-    }
-
-    /**
-     * Parse the parameters.
+     * Parse the arguments.
      *
      * @return array
      */
@@ -100,25 +52,45 @@ class ParameterParser
                         strlen($parameter) - strlen($prefix)
                     ),
                 ];
-                $current_argument = 0;
-                $argument_count = count(
-                    (new \ReflectionFunction($closure))->getParameters()
-                ) - 1;
-                while ($current_argument < $argument_count) {
-                    $closure_arguments[] = $this->argv[$i + 1];
-                    $current_argument += 1;
+                $rFunction = new \ReflectionFunction($closure);
+                if ($rFunction->isVariadic()) {
+                    $i++;
+                    while (
+                        isset($this->argv[$i]) &&
+                        ($argument = $this->argv[$i]) != null &&
+                        ! $this->prefixExists($argument)
+                    ) {
+                        $closure_arguments[] = $argument;
+                        $i++;
+                    }
+                    $results[
+                        substr(
+                            $parameter,
+                            strlen($prefix),
+                            strlen($parameter) - strlen($prefix)
+                        )
+                    ] = $closure(...$closure_arguments);
+                } else {
+                    $current_argument = 0;
+                    $argument_count = count($rFunction->getParameters()) - 1;
+                    while ($current_argument < $argument_count) {
+                        $closure_arguments[] = $this->argv[$i + 1];
+                        $current_argument += 1;
+                        $i++;
+                    }
+                    $results[
+                        substr(
+                            $parameter,
+                            strlen($prefix),
+                            strlen($parameter) - strlen($prefix)
+                        )
+                    ] = $closure(...$closure_arguments);
                     $i++;
                 }
-                $results[
-                    substr(
-                        $parameter,
-                        strlen($prefix),
-                        strlen($parameter) - strlen($prefix)
-                    )
-                ] = $closure(...$closure_arguments);
-                $i++;
             } else {
-                $results['default'] = $this->prefixes->default->call($this, $parameter);
+                $results['default'] = $this->prefixes->default->call(
+                    $this, $parameter
+                );
                 $i++;
             }
         }
@@ -148,25 +120,33 @@ class ParameterParser
         $this->argv = [];
         while (($argument = array_shift($argv)) != null) {
             if (substr($argument, 0, 1) == '\'') {
-                $this->argv[] = substr($argument, 1);
-                while (
-                    ($argument_part = array_shift($argv)) != null &&
-                    substr($argument_part, strlen($argument_part) - 1, 1) != '\''
-                ) {
-                    $this->argv[count($this->argv) - 1] .= ' '.$argument_part;
+                if (substr($argument, strlen($argument) - 1, 1) !== '\'') {
+                    $this->argv[] = substr($argument, 1);
+                    while (
+                        ($argument_part = array_shift($argv)) != null &&
+                        substr($argument_part, strlen($argument_part) - 1, 1) != '\''
+                    ) {
+                        $this->argv[count($this->argv) - 1] .= ' '.$argument_part;
+                    }
+                    $this->argv[count($this->argv) - 1] .=
+                    ' '.substr($argument_part, 0, strlen($argument_part) - 1);
+                } else {
+                    $this->argv[] = substr(substr($argument, 1), 0, strlen($argument) - 2);
                 }
-                $this->argv[count($this->argv) - 1] .=
-                ' '.substr($argument_part, 0, strlen($argument_part) - 1);
             } elseif (substr($argument, 0, 1) == '"') {
-                $this->argv[] = substr($argument, 1);
-                while (
-                    ($argument_part = array_shift($argv)) != null &&
-                    substr($argument_part, strlen($argument_part) - 1, 1) != '"'
-                ) {
-                    $this->argv[count($this->argv) - 1] .= ' '.$argument_part;
+                if (substr($argument, strlen($argument) - 1, 1) !== '\'') {
+                    $this->argv[] = substr($argument, 1);
+                    while (
+                        ($argument_part = array_shift($argv)) != null &&
+                        substr($argument_part, strlen($argument_part) - 1, 1) != '"'
+                    ) {
+                        $this->argv[count($this->argv) - 1] .= ' '.$argument_part;
+                    }
+                    $this->argv[count($this->argv) - 1] .=
+                    ' '.substr($argument_part, 0, strlen($argument_part) - 1);
+                } else {
+                    $this->argv[] = substr(substr($argument, 1), 0, strlen($argument) - 2);   
                 }
-                $this->argv[count($this->argv) - 1] .=
-                ' '.substr($argument_part, 0, strlen($argument_part) - 1);
             } else {
                 $this->argv[] = $argument;
             }
