d7696384c2baf7d32cd03a17352d060303077681
nathan-fiscaletti <nathan.fiscaletti@gmail.com>
2016-11-09 15:10:18 -0800

    Fixed indentation in functions file

--- src/ParameterParser/Functions.php
+++ src/ParameterParser/Functions.php
@@ -7,16 +7,16 @@
  */
 
 if (!function_exists('parameter')) {
-	/**
-	 * Creates a new ParameterClosure.
-	 *
-	 * @param  string   $parameterName
-	 * @param  \Closure $closure
-	 *
-	 * @return \ParameterParser\ParameterClosure
-	 */
-	function parameter($parameterName, \Closure $closure)
-	{
-		return new \ParameterParser\ParameterClosure($parameterName, $closure);
-	}
+    /**
+     * Creates a new ParameterClosure.
+     *
+     * @param  string   $parameterName
+     * @param  \Closure $closure
+     *
+     * @return \ParameterParser\ParameterClosure
+     */
+    function parameter($parameterName, \Closure $closure)
+    {
+        return new \ParameterParser\ParameterClosure($parameterName, $closure);
+    }
 }
