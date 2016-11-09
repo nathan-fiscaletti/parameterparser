972a41c926fd117faab60f2fb9b38d85cb41ad6d
Nathan Fiscaletti <nathan.fiscaletti@stackpath.com>
2016-11-09 15:09:03 -0800

    Added `parameter` helper function.

---
+++ src/ParameterParser/Functions.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ |--------------------------------------------------------------------------
+ | Helper Functions
+ |--------------------------------------------------------------------------
+ */
+
+if (!function_exists('parameter')) {
+	/**
+	 * Creates a new ParameterClosure.
+	 *
+	 * @param  string   $parameterName
+	 * @param  \Closure $closure
+	 *
+	 * @return \ParameterParser\ParameterClosure
+	 */
+	function parameter($parameterName, \Closure $closure)
+	{
+		return new \ParameterParser\ParameterClosure($parameterName, $closure);
+	}
+}
