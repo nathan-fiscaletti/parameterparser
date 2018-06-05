be6d274a9f7c3d0685253fbd24e49674e5a48ad6
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-06-05 09:00:29 -0500

    Added ParameterParser halting functionality, added ParameterResult class, added helper functions for ParameterResult.

---
+++ src/ParameterParser/ParameterResult.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ParameterParser;
+
+class ParameterResult
+{
+    /**
+     * Resulting value.
+     *
+     * @var mixed
+     */
+    private $value = null;
+
+    /**
+     * If set to true, the parser will be halted.
+     *
+     * @var boolean
+     */
+    private $shouldHalt = false;
+
+    /**
+     * Construct the parameter result with a value
+     * and a halt value.
+     *
+     * @param mixed $value
+     * @param bool  $halt
+     */
+    public function __construct($value, $halt = false)
+    {
+        $this->value = $value;
+        $this->shouldHalt = $halt;
+    }
+
+    /**
+     * Retrieve the value of the ParameterResult.
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Check if this ParameterResult should ONLY halt the parser.
+     *
+     * @return bool
+     */
+    public function isHaltOnly()
+    {
+        return $this->value == ParameterParser::HALT_PARSE;
+    }
+
+    /**
+     * Check if apart of the result includes halting
+     * the parser.
+     *
+     * @return bool
+     */
+    public function shouldHalt()
+    {
+       return $this->shouldHalt;
+    }
+}
